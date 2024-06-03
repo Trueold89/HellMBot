015d939bd5dc32ac3fd1615004bbbe5b9c00ed34
trueold89 <trueold89@orudo.ru>
2024-06-03 19:10:34 +0300

    Init setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name="HellMBot",
+    version="0.1",
+    url="https://git.orudo.ru/trueold89/HellMBot",
+    author="trueold89",
+    author_email="trueold89@orudo.ru",
+    description="Discord bot that will wake your friends up from full mute by putting them through 9 circles of hell",
+    packages=["hellmbot"],
+    long_description=open("README.md").read(),
+    install_requires=["discord.py"],
+    entry_points={
+        "console_scripts": ["hellm = hellmbot.bot:main"]
+    }
+)
