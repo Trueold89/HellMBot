f5f9d86a1eccbfd5cf7c3032309f0b3bab917f20
trueold89 <trueold89@orudo.ru>
2025-03-21 15:58:15 +0300

    Update docker - Rewrite Dockerfile - Update docker-compose - Update setup.py - Add .dockerignore

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 
 setup(
     name="HellMBot",
-    version="0.2",
+    version="0.3",
     url="https://git.orudo.ru/trueold89/HellMBot",
     author="trueold89",
     author_email="trueold89@orudo.ru",
     description="Discord bot that will wake your friends up from full mute by putting them through 9 circles of hell",
     packages=["hellmbot"],
-    install_requires=["discord.py", "pydantic", "pydantic_settings"],
+    install_requires=["discord.py", "pydantic", "pydantic_settings", "aiosqlite"],
     entry_points={
         "console_scripts": ["hellm = hellmbot.bot:start"]
     }
